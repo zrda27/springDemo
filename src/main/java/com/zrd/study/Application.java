fce0bd53d249e73eb709e5605e850ac7874a2dfe
zengruda <zrda27@163.com>
2017-04-20 11:02:50 +0800

    'm'

--- src/main/java/com/zrd/study/Application.java
+++ src/main/java/com/zrd/study/Application.java
@@ -3,13 +3,15 @@ package com.zrd.study;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 	@Autowired
 	private HelloVO hello;
 
@@ -21,6 +23,11 @@ public class Application {
 	@RequestMapping("hh")
 	String hh(Model model){
 		return "hh";
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(Application.class);
 	}
 
 	public static void main(String[] args) {
